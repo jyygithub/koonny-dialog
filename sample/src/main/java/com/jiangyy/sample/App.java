9ea3c69eb5ff4091c53e801ede881ea60f3c2c5d
jiangyunyang <jiangyychn@163.com>
2017-08-29 17:27:51 +0800

    新添自定义对话框Style

--- sample/src/main/java/com/jiangyy/sample/App.java
+++ sample/src/main/java/com/jiangyy/sample/App.java
@@ -12,6 +12,6 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        DialogUtils.initStyle(R.style.DialogTheme);
+        DialogUtils.initStyle(R.style.Theme_AppCompat_DayNight_Dialog_Alert);
     }
 }
