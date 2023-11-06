ba1e3ab356a8f56959d68dded3a5cca20275721c
jiangyy <jiangyy@petalmail.com>
2023-11-06 14:59:32 +0800

    全新升级

--- sample/src/main/java/com/koonny/sample/MainActivity.kt
+++ sample/src/main/java/com/koonny/sample/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.koonny.dialog.Confirm2Dialog
 import com.koonny.dialog.ConfirmDialog
+import com.koonny.dialog.LoadingDialog
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val data = listOf("ConfirmDialog", "BottomListDialog<String>", "BottomListDialog<Any>", "BottomMenuDialog", "CustomDialog")
+        val data = listOf("ConfirmDialog", "Confirm2Dialog", "LoadingDialog")
         val adapter = MyAdapter(data)
         findViewById<RecyclerView>(R.id.recyclerView).adapter = adapter
         adapter.itemClick {
@@ -23,6 +25,18 @@ class MainActivity : AppCompatActivity() {
                         Toast.makeText(this@MainActivity, "Yes", Toast.LENGTH_SHORT).show()
                     }
                     .show(supportFragmentManager)
+
+                1 -> Confirm2Dialog.get()
+                    .setTitle("发现新版本")
+                    .setMessage("是否下载新版本？")
+                    .setPositive("下载并安装")
+                    .setNegative("忽略")
+                    .positiveClick {
+                        Toast.makeText(this@MainActivity, "Yes", Toast.LENGTH_SHORT).show()
+                    }
+                    .show(supportFragmentManager)
+
+                2 -> LoadingDialog.get().setMessage("加载...").show(supportFragmentManager)
             }
         }
 
